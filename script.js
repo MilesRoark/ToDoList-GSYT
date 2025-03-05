1b2248446b5a1d41419e769433f9f69beabccba5
MilesRoark <146493186+MilesRoark@users.noreply.github.com>
2025-03-05 04:47:10 -0500

    simple todo app

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const taskBtn = document.getElementById("taskBtn");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Really? Nothing? Alright, you're the boss.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+inputBox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
